41a1641fa1825a454e1788c44dc65be64ae22a5d
angelamaunz <alynnem0z@gmail.com>
2015-07-26 09:20:21 -0600

    1st commit finish section 1.

--- js/basics.js
+++ js/basics.js
@@ -1,3 +1,29 @@
-/**
- * Created by admin on 7/26/15.
- */
+// console.log used to view in dev tool - javascript
+
+console.log('<3');
+
+console.log(">3");
+
+
+/*
+Alert
+	Modal dialog box
+	Blocks the browser UI
+	Most basic form of debugging
+	Objects and arrays cannot be expanded
+	Favor console.log over alerts*/
+
+// alert('oh my');
+
+/*
+Variables
+	A container for a value
+	Javascript is a weakly/dynamically typed
+	No need to specify variables types
+	Variable types can change
+	Sometimes we don't know a variable type.*/
+
+// created without any value therefore it will automatically have the special value undefined.
+// If we declare a variable without using the var keyword it is automatically become a global variable even inside a function.
+
+var theMeaningOfLife = 42;
